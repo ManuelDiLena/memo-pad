067da1cb7802c27be9a97a1f1018954909bf2378
ManuelDiLena <manuel.dilena.29@gmail.com>
2024-02-26 20:07:17 +0100

    Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([{
+    id: 0,
+    title: 'First memo',
+    text: '# HelloWorld',
+    pinned: false,
+    created: Date.now()
+  }]);
+
   return (
-    <div className="App">
-      
+    <div className='App container'>
+      <div className='panel'>
+        <div className='menu'>
+          <input className='search' placeholder='Search' />
+          <button className='btn'>New Memo</button>
+        </div>
+        <div className='list'>
+          {
+            items.map((item, i) => {
+              return <div key={item.id}>{item.title}</div>
+            })
+          }
+        </div>
+      </div>
+
+      <div>
+        <div className='editor'></div>
+        <div className='preview'></div>
+      </div>
     </div>
   );
 }
