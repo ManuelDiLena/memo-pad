d3241cb717c8af1dbf881f782222aba6a07651f4
ManuelDiLena <manuel.dilena.29@gmail.com>
2024-02-26 20:11:09 +0100

    New memo

--- src/App.js
+++ src/App.js
@@ -10,17 +10,29 @@ function App() {
     created: Date.now()
   }]);
 
+  function handleClick() {
+    const memo = {
+      id: 1,
+      title: 'Second memo',
+      text: '# HelloWorld',
+      pinned: false,
+      created: Date.now()
+    }
+
+    setItems([...items, memo])
+  }
+
   return (
     <div className='App container'>
       <div className='panel'>
         <div className='menu'>
           <input className='search' placeholder='Search' />
-          <button className='btn'>New Memo</button>
+          <button className='btn' onClick={(e) => handleClick()}>New Memo</button>
         </div>
         <div className='list'>
           {
             items.map((item, i) => {
-              return <div key={item.id}>{item.title}</div>
+              return <div key={item.id} className='memo'>{item.title}</div>
             })
           }
         </div>
